bb17b9bfd077500745a698224a153fa756471cae
Razvan Rauta <rrauta@klika-tech.com>
2021-10-13 20:49:38 +0300

    More project setup

--- config-overrides.js
+++ config-overrides.js
@@ -5,12 +5,12 @@
  * @ Time: 17:39
  */
 
-const { override, addWebpackAlias } = require('customize-cra');
-const path = require('path');
+const { override, addWebpackAlias } = require('customize-cra')
+const path = require('path')
 
 module.exports = override(
   addWebpackAlias({
-    '@': path.resolve(__dirname, 'src'),
     '@mui/styled-engine': '@mui/styled-engine-sc',
+    '@': path.resolve(__dirname, 'src'),
   })
-);
+)
