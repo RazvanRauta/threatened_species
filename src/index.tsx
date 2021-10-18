658c0bc7c401a1772c87dd2cd3f1ff41e19328a7
Razvan Rauta <rrauta@klika-tech.com>
2021-10-18 19:20:42 +0300

    Added redux-persist, not sure if is a good idea

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import './index.css'
 
+import store, { persistor } from './store'
+
 import App from './App'
 import CssBaseline from '@mui/material/CssBaseline'
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from '@mui/material/styles'
-import store from './store'
 import theme from './theme'
 
 ReactDOM.render(
   <React.Fragment>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <CssBaseline />
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <CssBaseline />
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.Fragment>,
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,24 +5,55 @@
  */
 
 import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from 'redux-persist'
 
+import { combineReducers } from 'redux'
 import regionsReducer from './red-list/regions/regionsSlice'
 import speciesReducer from './red-list/species/speciesSlice'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['regions', 'species'],
+}
+
+const rootReducer = combineReducers({
+  regions: regionsReducer,
+  species: speciesReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-  reducer: {
-    regions: regionsReducer,
-    species: speciesReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
 export type AppDispatch = typeof store.dispatch
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof rootReducer>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
 >
+
+export const persistor = persistStore(store)
 
 export default store
